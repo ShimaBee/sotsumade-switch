3de4c04b90152dab4b65b75a8a771f2345fbde11
島袋雄太 <shimabukuroyuuta@shimabee.local>
2019-01-18 19:37:04 +0900

    add routing

--- app.rb
+++ app.rb
@@ -12,3 +12,15 @@ end
 get '/expert.erb' do
   erb :expert
 end
+
+get '/conform.erb' do
+  erb :conform
+end
+
+get '/graduate.erb' do
+  erb :graduate
+end
+
+get '/remainding-units.erb' do
+  erb :remainding-units
+end
