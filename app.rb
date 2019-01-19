0f1694ccda3d47d5ff3bba3542dfe83590d84b8d
島袋雄太 <shimabukuroyuuta@shimabee.local>
2019-01-19 18:01:28 +0900

    global変数使う

--- app.rb
+++ app.rb
@@ -10,15 +10,15 @@ get '/general' do
 end
 
 post '/expert' do
-  @health = params[:health].to_i
-  @humanity = params[:humanity].to_i
-  @society = params[:society].to_i
-  @nature = params[:nature].to_i
-  @synthesis = params[:synthesis].to_i
-  @ryudai = params[:ryudai].to_i
-  @information = params[:information].to_i
-  @language1 = params[:language1].to_i
-  @language2 = params[:language2].to_i
+  $health = params[:health].to_i
+  $humanity = params[:humanity].to_i
+  $society = params[:society].to_i
+  $nature = params[:nature].to_i
+  $synthesis = params[:synthesis].to_i
+  $ryudai = params[:ryudai].to_i
+  $information = params[:information].to_i
+  $language1 = params[:language1].to_i
+  $language2 = params[:language2].to_i
   erb :expert
 end
 
