bcead48c424d98fc317243d2182c99a5423faf01
島袋雄太 <mrs1122ani@gmail.com>
2019-01-23 23:46:35 +0900

    定数 -> インスタンス変数

--- app.rb
+++ app.rb
@@ -10,58 +10,58 @@ get '/general' do
 end
 
 post '/expert' do
-  HEALTH = params[:health].to_i
-  HUMANITY = params[:humanity].to_i
-  SOCIETY = params[:society].to_i
-  NATURE = params[:nature].to_i
-  SYNTHESIS = params[:synthesis].to_i
-  RYUDAI = params[:ryudai].to_i
-  INFORMATION_SCIENCE = params[:information_science].to_i
-  INFORMATION = params[:information].to_i
-  ENGLISH = params[:English].to_i
-  FOREIGN_LANGUAGE = params[:foreign_language].to_i
+  @health = params[:health].to_i
+  @humanity = params[:humanity].to_i
+  @society = params[:society].to_i
+  @nature = params[:nature].to_i
+  @synthesis = params[:synthesis].to_i
+  @ryudai = params[:ryudai].to_i
+  @information_science = params[:information_science].to_i
+  @information = params[:information].to_i
+  @english = params[:english].to_i
+  @foreign_language = params[:foreign_language].to_i
   erb :expert
 end
 
 post '/conform' do
-  COMPULSORY_SUBJECTS = params[:compulsory_subjects].to_i
-  COMPULSORY_ELECTIVE = params[:compulsory_elective].to_i
-  DEPARTMENT_UNIQUE = params[:department_unique].to_i
-  DEPARTMENT_ELECTIVE = params[:department_elective].to_i
-  OTHER_FACULTY = params[:other_faculty].to_i
+  @compulsory_subjects = params[:compulsory_subjects].to_i
+  @compulsory_elective = params[:compulsory_elective].to_i
+  @department_unique = params[:department_unique].to_i
+  @department_elective = params[:department_elective].to_i
+  @other_faculty = params[:other_faculty].to_i
 
   erb :conform
 end
 
 get '/decision' do
-  @other_liberal_arts = SYNTHESIS + RYUDAI + INFORMATION_SCIENCE + INFORMATION
-  @elective_units = OTHER_FACULTY
-  @expert_units = COMPULSORY_SUBJECTS + COMPULSORY_ELECTIVE + DEPARTMENT_UNIQUE + DEPARTMENT_ELECTIVE
-  GENERAL_UNITS = HEALTH + HUMANITY + SOCIETY + NATURE + SYNTHESIS + RYUDAI + INFORMATION + INFORMATION_SCIENCE + ENGLISH + FOREIGN_LANGUAGE
+  @other_liberal_arts = @synthesis + @ryudai + @information_science + @information
+  @elective_units = @other_faculty
+  @expert_units = @compulsory_subjects + @compulsory_elective + @department_unique + @department_elective
+  @general_units = @health + @humanity + @society + @nature + @synthesis + @ryudai + @information_science + @information + @english + @foreign_language
 
-  if HUMANITY > 4
-    @other_liberal_arts += (HUMANITY - 4)
+  if @humanity > 4
+    @other_liberal_arts += (@humanity - 4)
   end
 
-  if SOCIETY > 4
-    @other_liberal_arts += (SOCIETY - 4)
+  if @society > 4
+    @other_liberal_arts += (@society - 4)
   end
 
-  if NATURE > 4
-    @other_liberal_arts += (NATURE - 4)
+  if @nature > 4
+    @other_liberal_arts += (@nature - 4)
   end
 
 
-  if GENERAL_UNITS > 32 && GENERAL_UNITS <= 42
-    @elective_units += (GENERAL_UNITS - 32)
-  elsif GENERAL_UNITS > 42
+  if @general_units > 32 && @general_units <= 42
+    @elective_units += (@general_units - 32)
+  elsif @general_units > 42
     @elective_units += 10
   else
     @elective_units
   end
 
-  if ENGLISH < 10
-    @elective_units -= (10 - ENGLISH)
+  if @english < 10
+    @elective_units -= (10 - @english)
   end
 
   if @expert_units > 54
@@ -69,7 +69,7 @@ get '/decision' do
   end
 
   @total_expert_units = @expert_units + @elective_units
-  @total_units = GENERAL_UNITS + @expert_units + @elective_units
+  @total_units = @general_units + @expert_units + @elective_units
 
   erb :graduation
 end
